9dd704478ffa03335123026a2ec9637aa0a0246b
Vertuhkov Yaroslav <yvertukhov@extyl-pro.ru>
2021-09-16 11:50:19 +0300

    fix

--- src/components/map-viewer.vue
+++ src/components/map-viewer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-html="svgStr"></div>
+  <div ref="mapViewer" v-html="svgStr"></div>
 </template>
 
 <script>
@@ -9,6 +9,37 @@ export default {
     svgStr: {
       type: String,
       default: '<svg></svg>',
+    },
+  },
+  data() {
+    return {
+      svgEl: null,
+    };
+  },
+  mounted() {
+    this.svgEl = this.$refs.mapViewer.querySelector('svg');
+    this.addEventClick();
+  },
+  methods: {
+    addEventClick() {
+      this.svgEl.addEventListener('click', (e) => {
+        // const targetCoords = this.svgEl.getBoundingClientRect();
+        const NS = this.svgEl.getAttribute('xmlns');
+        const point = this.svgEl.createSVGPoint();
+        point.x = e.clientX;
+        point.y = e.clientY;
+        const svgP = point.matrixTransform(this.svgEl.getScreenCTM().inverse());
+
+        const circle = document.createElementNS(NS, 'circle');
+        circle.setAttribute('cx', svgP.x);
+        circle.setAttribute('cy', svgP.y);
+        circle.setAttribute('r', 10);
+
+        this.svgEl.appendChild(circle);
+
+        // console.log(e.clientX - targetCoords.left);
+        // console.log(e.clientY - targetCoords.top);
+      });
     },
   },
 };
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
+import ElementUI from 'element-ui';
 import api from './plugins/api';
 import App from './app.vue';
 import router from './router';
 import store from './store';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.use(api);
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
 
